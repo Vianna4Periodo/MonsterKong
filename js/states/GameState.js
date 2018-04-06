76a0468335ebca5556ef4defd85677b34c649215
aluno <aluno@labivj.info>
2018-04-06 20:32:55 -0300

    Pulando

--- js/states/GameState.js
+++ js/states/GameState.js
@@ -24,8 +24,6 @@ MonsterKong.GameState = {
         this.load.spritesheet('fire', 'assets/images/fire_spritesheet.png', 20, 21, 2, 1, 1);
 
         this.load.text('level', 'assets/data/level.json');
-
-
     },
 
     create: function () {
